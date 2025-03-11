ce6141292f97d2ea8c03384d2a047471a4ed418e
Glory <Esssodeou.Danigue@etu.unige.ch>
2025-03-11 21:58:58 +0100

    feat: Add JavaScript functionality to count from 1 to 10

---
+++ main.js
@@ -0,0 +1,29 @@
+function	countToTen(){
+    var	count	=	0;
+    while	(count	<	10)	{
+                count++;
+            
+document.getElementById("theCount").innerHTML
++=	count	+	"<br>";
+    }
+}
+// var firsNuber = prompt("Enter the first number");
+// var secondNumber = prompt("Enter the second number");
+// var result = Number(firsNuber) + Number(secondNumber);
+// alert("The result is: " + result);
+
+var movie = "le parrain";
+    function showMovie(){
+        alert(movie + " est un film de mafia");
+    }
+    function showBadMovie(){
+        movie = "Twilight";
+        alert(movie + " est un film de vampires");
+    }
+    const height = 10;
+    const speedOfLight = 299792458;
+    const PI = 3.14159;
+    const numberOfProblems = 99;
+    const numberOfPlanets = 8;
+
+    
